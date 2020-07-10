4b828ae3b8c8d71080683c7cf68a4804e1b82e0a
bedril.moussakat <bedril.moussakat@accenture.com>
2020-07-10 18:49:29 +0400

    deploy.sh ajout de l'entete

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+#####################################################
+#
+#   Description : Déploiement à la volée de conteneur
+#
+#   Auteur : Bedril 
+#
+#   Date  : 10/07/2020
+#####################################################
+
+echo "
+ 
+Options : 
+        - --create : lancer des conteneurs
+
+        - --drop : supprimer des conteneurs créer par le script deploy.sh
+
+        - --infos : caracteristique des conteneurs [ip,nom,user, ...]
+
+        - --start : redemarrage des conteneurs
+
+        - --ansible : deploiement arborescence ansible
+"
+
